24efe8b009cd43ac64a469b79428315f593c3cf1
YuitoSakano <133097663+SakanoYuito@users.noreply.github.com>
2023-06-13 15:23:20 +0900

    うおおお

--- main.go
+++ main.go
@@ -1,7 +1,91 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
+)
+
+type City struct {
+	ID          int    `json:"id,omitempty"  db:"ID"`
+	Name        string `json:"name,omitempty"  db:"Name"`
+	CountryCode string `json:"countryCode,omitempty"  db:"CountryCode"`
+	District    string `json:"district,omitempty"  db:"District"`
+	Population  int    `json:"population,omitempty"  db:"Population"`
+}
+
+var (
+	db *sqlx.DB
+)
 
 func main() {
-	fmt.Println("なろう講習会へようこそ！")
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conf := mysql.Config{
+		User:      os.Getenv("DB_USERNAME"),
+		Passwd:    os.Getenv("DB_PASSWORD"),
+		Net:       "tcp",
+		Addr:      os.Getenv("DB_HOSTNAME") + ":" + os.Getenv("DB_PORT"),
+		DBName:    os.Getenv("DB_DATABASE"),
+		ParseTime: true,
+		Collation: "utf8mb4_unicode_ci",
+		Loc:       jst,
+	}
+
+	_db, err := sqlx.Open("mysql", conf.FormatDSN())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("conntected")
+	db = _db
+
+	e := echo.New()
+
+	e.GET("/cities/:cityName", getCityInfoHandler)
+	e.POST("/cities/newCity", newCityHandler)
+
+	e.Start(":8000")
+}
+
+func getCityInfoHandler(c echo.Context) error {
+	cityName := c.Param("cityName")
+	fmt.Println(cityName)
+
+	var city City
+	if err := db.Get(&city, "SELECT * FROM city WHERE Name=?", cityName); errors.Is(err, sql.ErrNoRows) {
+		return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("No such city Name = %s", cityName))
+	} else if err != nil {
+		log.Fatalf("DB Error: %s", err)
+	}
+
+	return c.JSON(http.StatusOK, city)
+}
+
+func newCityHandler(c echo.Context) error {
+	city := &City{}
+	err := c.Bind(city)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "bad request body")
+	}
+
+	_, err = db.Exec("INSERT INTO city (Name, CountryCode, District, Population) VALUES (?, ?, ?, ?);", city.Name, city.CountryCode, city.District, city.Population)
+
+	if err != nil {
+		log.Fatalf("DB Error: %s", err) 
+	}
+
+	return c.JSON(http.StatusOK, city)
+
 }
